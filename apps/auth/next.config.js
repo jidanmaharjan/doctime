0acafacfa3c0f69b41a6f3c1bd362ec3309f591b
jidanmaharjan <jidanmaharjan1@gmail.com>
2025-07-24 17:01:57 +0545

    changes to config and components json

--- apps/auth/next.config.js
+++ apps/auth/next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  cookies: {
-    sessionToken: {
-      name: "__Secure-next-auth.session-token",
-      options: {
-        domain: ".vercel.app", // <-- share session across subdomains
-        path: "/",
-        httpOnly: true,
-        secure: true,
-        sameSite: "lax",
-      },
-    },
-  },
+  // cookies: {
+  //   sessionToken: {
+  //     name: "__Secure-next-auth.session-token",
+  //     options: {
+  //       domain: ".vercel.app", // <-- share session across subdomains
+  //       path: "/",
+  //       httpOnly: true,
+  //       secure: true,
+  //       sameSite: "lax",
+  //     },
+  //   },
+  // },
 };
 
 export default nextConfig;
